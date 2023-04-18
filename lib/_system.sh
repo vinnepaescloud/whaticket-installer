033219361db62fa03e0d7e14e0040ff5754a38c3
JokerSSH <101794296+JokerSSH@users.noreply.github.com>
2023-04-17 21:28:12 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/melissagobatti/install_siwhaticket_saas /home/deploy/whaticket/
+  git clone https://github.com/slopesweb/saascodigofinan /home/deploy/whaticket/
 EOF
 
   sleep 2
